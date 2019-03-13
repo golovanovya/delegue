4ecf0ff7e98482fb07f273bd8c5f8ecc828746ea
Yury Golovanov <dkts-uagolovanov@osk.rzd>
2019-03-13 11:52:55 +0300

    add test controller

---
+++ models/Test.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "test".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ */
+class Test extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'test';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['content'], 'string'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+        ];
+    }
+}
